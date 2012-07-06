cbf0d54635dcf4843980c2c53c58dcb6c50d9198
Alex Koloskov <alexandr.koloskov@gmail.com>
2012-07-06 10:08:58 -0700

    Modified script to add vim bundles as submodules automatically

--- update_vim_bundles.sh
+++ update_vim_bundles.sh
@@ -1,1 +1,8 @@
 vim +BundleInstall +qall
+
+for dir in $(find vim/bundle -type d -depth 1 -not -name *vundle); do
+  cd $dir
+  repo_url=`git config --get remote.origin.url`
+  cd ../../..
+  git submodule add $repo_url $dir
+done
