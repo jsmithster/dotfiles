9d92bcfdbb6671b425d9703a91cd654fff6412bc
Alex Koloskov <alexandr.koloskov@gmail.com>
2012-08-31 22:09:01 -0700

    Catching up

--- zsh/config/path.zsh
+++ zsh/config/path.zsh
@@ -3,3 +3,4 @@ export ANDROID_NDK_ROOT=~/Dev/-sdk/android-ndk-r8
 
 export PATH=$PATH:$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/platform-tools
 export PATH=$PATH:$ANDROID_NDK_ROOT
+export PATH=$PATH:~/Config/dotfiles/bin
